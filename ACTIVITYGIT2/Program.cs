0058c79b326b9b12d1a35332c90bf275c76a0ae0
angel <a2308744@institutmontilivi.cat>
2024-01-31 20:04:27 +0100

    sfdffwer

--- ACTIVITYGIT2/Program.cs
+++ ACTIVITYGIT2/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Victor!");
             Console.WriteLine("Hello, Patata!");
+            Console.WriteLine(2);
         }
     }
 }
